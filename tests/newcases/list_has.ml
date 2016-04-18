fe255c4a4bb490986bd61011624155464aaae151
hachiko99 <qinanj22@gmail.com>
2016-04-17 23:10:23 -0400

    New examples

--- tests/newcases/list_has.ml
+++ tests/newcases/list_has.ml
@@ -11,17 +11,11 @@ type bool =
   | False
 
 
-let list_has : list -> nat -> bool |>
-  { [] => ( 0 => False
-          | 1 => False
-          | 2 => False)
-  | [0] => ( 0 => True
-           | 1 => False)
-  | [1] => ( 1 => True
-           | 0 => False)
-  | [0; 1] => ( 0 => True
-              | 1 => True)
-  | [2] => ( 2 => True
-           | 1 => False)
-  | [2; 1] => ( 2 => True
-              | 1 => True)} = ?
+let list_has : nat -> list -> bool |>
+  { 0 => ( [] => False
+         | [0] => True
+         | [0; 1] => True
+         | [1] => False)
+  | 1 => ( [] => False
+         | [0; 2] => False
+         | [0; 1] => True)} = ?
