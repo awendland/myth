237067217e08493f6a0913b99faa6473bfa878f4
hachiko99 <qinanj22@gmail.com>
2016-04-19 18:17:17 -0400

    Add new examples Add test cases with modified examples Add report folder

--- tests/newcases/list_index.ml
+++ tests/newcases/list_index.ml
@@ -6,15 +6,29 @@ type list =
   | Nil
   | Cons of nat * list
 
+type bool =
+  | True
+  | False
+
+let rec nat_equal (n1 : nat) (n2 :nat) : bool =
+  match n1 with
+  | O -> (match n2 with
+             | O -> True
+             | S (m) -> False
+           )
+  | S (m1) -> ( match n2 with
+        | O -> False
+        | S (m2) -> (nat_equal m1 m2) )
+;;
+
 let list_index : nat -> list -> nat |>
   { 0 => ( [] => 0
          | [0] => 0
-         | [0; 1] => 0
-         | [2; 1 ;0] => 2
+         | [3; 0] => 1
+         | [2; 3; 0] => 2
          | [1; 2; 3; 0] => 3)
   | 1 => ( [] => 0
-         | [1; 0] => 0
-         | [2; 1] => 1
-         | [5; 4; 3; 2; 1] => 4)
-  | 2 => ( [1; 2] => 1
-         | [0; 1; 2] => 2)} = ?
+         | [2] => 0
+         | [1; 2] => 0
+         | [4; 1; 2] => 1
+         | [3; 4; 1; 2] => 2)} = ?
